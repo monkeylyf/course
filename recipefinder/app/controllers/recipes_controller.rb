82380a4f946ba14168dcae976c011b3af6a1a59f
monkeylyf <laituan1986@gmail.com>
2015-10-14 16:48:31 -0700

    recipefinder completed course 1

--- recipefinder/app/controllers/recipes_controller.rb
+++ recipefinder/app/controllers/recipes_controller.rb
@@ -1,4 +1,34 @@
+require 'httparty'
+require 'pp'
+
+
 class RecipesController < ApplicationController
   def index
+    search_term = params[:search] || 'chocolate'
+    Rails.logger.debug("My object: #{@RecipeFetcher.inspect}")
+    http_resp = RecipeFetcher.for(@search_term)
+    Rails.logger.debug("My object: #{@http_resp.inspect}")
+    hash_response = JSON.parse(http_resp)
+    @recipes = hash_response['recipes']
   end
 end
+
+
+
+class RecipeFetcher
+  include HTTParty
+
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+  default_params key: key_value
+  format :json
+
+  def self.for term
+    get("/search", query: {q: term})
+  end
+end
+
+
+pp RecipeFetcher.for('chocolate')
+
--- recipefinder/app/models/recipe_fetcher.rb
+++ recipefinder/app/models/recipe_fetcher.rb
@@ -1,18 +1,20 @@
+#class Recipe < ActiveRecord::Base
 require 'httparty'
 require 'pp'
 
-
-class Recipe
+class RecipeFetcher
   include HTTParty
 
-  @key_value = ENV['FOOD2FORK_KEY']
+  key_value = ENV['FOOD2FORK_KEY']
   hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
   base_uri "http://#{hostport}/api"
+  default_params key: key_value
   format :json
 
   def self.for term
-    get("/search", query: {key: @key_value, q: term})
+    get("/search", query: {q: term})
   end
 end
 
-pp Recipe.for 'chocolate'
+
+pp RecipeFetcher.for('chocolate')
